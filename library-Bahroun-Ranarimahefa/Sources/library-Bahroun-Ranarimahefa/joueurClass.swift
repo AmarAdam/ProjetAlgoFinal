317bdd0392d00904e5ba9999e8ad76776e4bec21
Amjad Menouer <32568925+AmjadIG@users.noreply.github.com>
2018-12-17 09:35:46 +0100

    partie + piece + joueur

---
+++ library-Bahroun-Ranarimahefa/Sources/library-Bahroun-Ranarimahefa/joueurClass.swift
@@ -0,0 +1,29 @@
+//Classe Joueur
+//Done
+fileprivate class Joueur {
+    var namePlayer : String
+    var collectionPiece : CollectionPiece
+    var reserve : Reserve
+
+    // Constructeur de Joueur
+    init(_ nom : String) {
+      self.namePlayer = nom
+      self.collectionPiece = CollectionPiece() //import collectionPieceClass
+      self.reserve = Reserve() //import reserveClass
+    }
+
+    // getter Reserve
+    func getReserve() -> Reserve {
+      return self.reserve
+    }
+
+    // getter Nom Joueur
+    func getNomJoueur() -> String {
+      return self.namePlayer
+    }
+
+    // getter CollectionPiece
+    func getCollectionPieceJoueur() -> CollectionPiece {
+      return self.collectionPiece
+    }
+}
---
+++ library-Bahroun-Ranarimahefa/Sources/library-Bahroun-Ranarimahefa/pieceClass.swift
@@ -0,0 +1,69 @@
+/*
+
+Reserve => Liste de Piece
+CollectionPiece => Liste de Piece
+Partie => classe
+  * xMax
+  * yMax
+  //On ne fera pas de tableau : on consultera directement les pieces a partir des joueurs de Partie
+  * Joueur (j1)
+  * Joueur (j2)
+*/
+
+//Classe Piece
+fileprivate class Piece {
+    var name : String
+    var position : Int?
+
+    // Constructeur de Piece
+    init(_ nom : String , _ position : Int ) {
+      self.name = nom
+      self.position = position
+    }
+
+    // getter NomPiece
+    func getNomPiece() -> String {
+      return self.name
+    }
+
+    // setter NomPiece
+    func setNomPiece(_ nom : String) -> Self {
+      self.name = nom
+      return self
+    }
+
+    // getter Position
+    func getPosition()-> Int? {
+      return self.position
+    }
+
+    // True si la piece est un roi
+    func estRoi() -> Bool {
+      return (self.name == "koropokkuru")
+    }
+
+    // True si la piece est un kodama
+    func estKodama() -> Bool {
+      return (self.name == "kodama")
+    }
+
+    // True si la piece est un kodama samurai
+    func estKodamaSamurai() -> Bool {
+      return (self.name == "kodama samurai")
+    }
+
+    // // Transforme la piece kodama en kodama samurai
+    // func transformerEnKodamaSamurai() throws {
+    // }
+    //
+    // // Transforme le kodama samurai en kodama
+    // func transformerEnKodama() throws {
+    //   do {
+    //     if self.estKodama() {
+    //       try self.setNomPiece("kodama")
+    //     }
+    //   } catch fatalError {
+    //       "Impossible"
+    //   }
+    // }
+}
