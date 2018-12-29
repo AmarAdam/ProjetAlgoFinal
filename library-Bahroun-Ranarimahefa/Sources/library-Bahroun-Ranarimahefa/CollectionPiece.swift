27430a0fe77b2d88b8f46bec6372a2a61f142f49
AmarAdam <adammegherat@gmail.com>
2018-12-29 18:37:18 +0100

    commit iterateur Reserve

--- library-Bahroun-Ranarimahefa/Sources/library-Bahroun-Ranarimahefa/CollectionPiece.swift
+++ library-Bahroun-Ranarimahefa/Sources/library-Bahroun-Ranarimahefa/CollectionPiece.swift
@@ -1,12 +1,33 @@
 fileprivate class CollectionPiece {
-
+    
+    typealias ItCollectionPiece = IteratorProtocol 
     var collection = [Piece]
 
     init() {
         // No action
     }
 
-     func ajouterCollectionPiece(_ piece: Piece) -> Self {
+    public struct ItCollectionPiece : IteratorProtocol{
+
+        typealias ItCollectionPiece = IteratorProtocol 
+
+        private let collection : CollectionPiece
+        private var courant : Int = 0
+
+        fileprivate init(_c : CollectionPiece){
+            self.CollectionPiece = c 
+        }
+
+        public func next() -> Piece? {
+            guard self.courant < self.collection.count else { return nil }
+            let val = self.courant
+            self.courant = self.courant + 1
+            return self.collection[val]
+        }
+    }
+     
+
+    func ajouterCollectionPiece(_ piece: Piece) -> Self {
          self.collection.append(piece)
      }
 
