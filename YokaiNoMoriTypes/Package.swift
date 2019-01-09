68371178b0d672a47b4b8c8d81e9c25a7dcd5e63
PepitoSama <etienne.saimond@gmail.com>
2019-01-09 09:20:21 +0100

    Modification du fichier manifest

--- YokaiNoMoriTypes/Package.swift
+++ YokaiNoMoriTypes/Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "library-Bahroun-Ranarimahefa",
+    name: "YokaiNoMoriTypes",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
