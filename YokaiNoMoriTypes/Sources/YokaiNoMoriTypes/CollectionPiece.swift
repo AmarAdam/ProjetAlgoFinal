736731e9014cb3b8d3d3408a3d0d56437fc31d4f
Adam <adammegherat@gmail.com>
2019-01-09 11:22:06 +0100

    iterator collectionpiece ok

--- YokaiNoMoriTypes/Sources/YokaiNoMoriTypes/CollectionPiece.swift
+++ YokaiNoMoriTypes/Sources/YokaiNoMoriTypes/CollectionPiece.swift
@@ -1,23 +1,16 @@
 import YokaiNoMoriTypes
 
-fileprivate class CollectionPiece {
+public struct ItCollectionPiece : IteratorProtocol{
 
-    typealias ItCollectionPiece = IteratorProtocol
-    var collection = [Piece]
-
-    init() {
-        // No action
-    }
-
-    public struct ItCollectionPiece : IteratorProtocol{
-
-        typealias ItCollectionPiece = IteratorProtocol
+        typealias ItCollectionPiece = CollectionPieceProtocol
 
         private let collection : CollectionPiece
         private var courant : Int = 0
+        private let pieces : [Piece]
 
         fileprivate init(_c : CollectionPiece){
             self.CollectionPiece = c
+            self.pieces = self.CollectionPiece.collection
         }
 
         public func next() -> Piece? {
@@ -26,6 +19,16 @@ fileprivate class CollectionPiece {
             self.courant = self.courant + 1
             return self.collection[val]
         }
+    }
+
+
+fileprivate class CollectionPiece {
+
+    typealias ItCollectionPiece = IteratorProtocol
+    var collection = [Piece]
+
+    init() {
+        // No action
     }
 
 
